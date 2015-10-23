f9aa6f9903d85ec9082456afe32b387e04e6b657
Ismael Celis <ismaelct@gmail.com>
2015-10-24 00:27:15 +0100

    Lets make MemlLock thread-safe while we’re at it.

--- lib/faraday_throttler/mem_lock.rb
+++ lib/faraday_throttler/mem_lock.rb
@@ -2,22 +2,25 @@ module FaradayThrottler
   class MemLock
     def initialize
       @locks = {}
+      @mutex = Mutex.new
     end
 
     def set(key, ttl = 30)
-      now = Time.now
-      exp = locks[key]
+      mutex.synchronize {
+        now = Time.now
+        exp = locks[key]
 
-      if !exp || exp < now
-        locks[key] = now + ttl
-        return true
-      else
-        return false
-      end
+        if !exp || exp < now
+          locks[key] = now + ttl
+          return true
+        else
+          return false
+        end
+      }
     end
 
     private
-    attr_reader :locks
+    attr_reader :locks, :mutex
 
   end
 end
